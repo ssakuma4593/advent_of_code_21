e40f00bd9a880db7f745c9965248dcf95e716cad
ssakuma4593 <satoe.sakuma@gmail.com>
2021-12-05 19:14:38 -0500

    clean up day 2

--- day_2.py
+++ day_2.py
@@ -1,16 +1,46 @@
-def day_2_part_1(instructions: list) -> tuple:
+def day_2_part_1(instructions: list) -> int:
     horizontal = 0
     depth = 0
 
     for instruction, units in instructions:
+        print(instruction, units)
+        unit = int(units)
         if instruction == "forward":
-            horizontal += int(units)
+            # print("--Forward", horizontal, "+", unit)
+            horizontal += unit
         elif instruction == "down":
-            depth += int(units)
+            # print("--Depth", depth, "+", unit)
+            depth += unit
         elif instruction == "up":
-            depth -= int(units)
+            # print("--Depth", depth, "-", unit)
+            depth -= unit
     
-    return (horizontal, depth)
+    return (horizontal * depth)
+
+def day_2_part_2(instructions: list) -> int:
+    horizontal = 0
+    depth = 0
+    aim = 0
+
+    for instruction, units in instructions:
+        print(instruction, units)
+        unit = int(units)
+        if instruction == "forward":
+            # print("--Forward", horizontal, "+", unit)
+            # print("--Depth", depth, "+", aim, "*", unit)
+            horizontal += unit
+            depth = (aim * unit) + depth
+            # print("Horizontal: ", horizontal, "Depth: ", depth)
+        elif instruction == "down":
+            # print("--Aim", aim, "+", unit)
+            aim += unit
+            # print("Depth: ", depth, "Aim: ", aim)
+        elif instruction == "up":
+            # print("--Aim", aim, "-", unit)
+            aim -= unit
+            # print("Depth: ", depth, "Aim: ", aim)
+    
+    return (horizontal * depth)
 
     
 
@@ -22,12 +52,13 @@ def main():
     print(insructions)
 
     # Part 1
-    horizontal, depth = day_2_part_1(insructions)
-    print("Horizontal: ", horizontal)
-    print("Depth: ", depth)
+    horizontal_depth_product = day_2_part_1(insructions)
+    print("Product: ", horizontal_depth_product)
 
+    # Part 2
+    horizontal_depth_product_2 = day_2_part_2(insructions)
+    print("Product 2: ", horizontal_depth_product_2)
     
-    # day_2_part_2(depths)
     fileObj.close()
 
         
